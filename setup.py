49c53e938abdb2754b7ae0017d6dd577a154834d
ukwhatn <ukwhatn@gmail.com>
2022-02-04 14:24:35 +0900

    edit setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ import setuptools
 setuptools.setup(
     name='wikidot',
     version='2.0.0',
-    description='Wikidot AMC/API Request Wrapper',
+    description='Wikidot Ajax/API Request Wrapper',
     author='ukwhatn',
     author_email='ukwhatn@gmail.com',
     url='https://github.com/SCP-JP/ukwhatn_wikidot.py',
     packages=setuptools.find_packages(),
-    python_requires='>=3.9',
+    python_requires='>=3.10',
     install_requires=[
         "bs4",
         "feedparser",
