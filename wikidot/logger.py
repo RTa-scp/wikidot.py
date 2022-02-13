b097e18dfe485c839891ac8a618d4a0abda00707
ukwhatn <ukwhatn@gmail.com>
2022-02-13 15:29:36 +0900

    change log format

--- wikidot/logger.py
+++ wikidot/logger.py
@@ -6,6 +6,6 @@ logger.setLevel(logging.WARNING)
 
 # StreamHandler
 _sh = logging.StreamHandler()
-_sh.setFormatter(logging.Formatter("[%(levelname)-8s/wikidot.py] %(message)s"))
+_sh.setFormatter(logging.Formatter("[wikidot.py/%(levelname)s] %(message)s"))
 logger.addHandler(_sh)
 del _sh
